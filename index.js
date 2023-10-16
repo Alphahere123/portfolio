df0d9d732c9cb39eaa73fee43a24b4ede7b86220
Adil ali <119725004+Alphahere123@users.noreply.github.com>
2023-10-16 14:52:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+
+//     $('menu').click(function(){
+//         $(this).toggleClass('fa-times')
+//         $('header').toggleClass('toggle')
+//     })
+//     $(window).on('scroll load',function(){
+//         $(this).toggleClass('fa-times')
+//         $('header').toggleClass('toggle')
+//     })
+// })
+
